24cd63b4fccf9c293bb6c426605754c2e5c51953
Moe Chaar <98048511+mokabc@users.noreply.github.com>
2022-07-31 19:07:43 -0700

    Update cart.jsx
    adding /api

--- cart.jsx
+++ cart.jsx
@@ -198,7 +198,7 @@ const Products = (props) => {
       <Row>
         <form
           onSubmit={(event) => {
-            restockProducts(`http://localhost:1337/${query}`);
+            restockProducts(`http://localhost:1337/api/${query}`);
             console.log(`Restock called on ${query}`);
             event.preventDefault();
           }}
